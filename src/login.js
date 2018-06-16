d419e6b33af473a7ac73fd8409833d4fb6a50cc7
Vlad_S <yavlads2014@yandex.ru>
2018-06-17 02:05:18 +0300

    release2

--- src/login.js
+++ src/login.js
@@ -1,15 +1,17 @@
 var loginInVK = function (){
 	      
 		  
-		  
-VK.Auth.getLoginStatus(function(resp) { 
-	if (resp.session) { 
+    VK.Auth.getLoginStatus(function(resp) { 
+	  if (resp.session) { 
+	  
 		pushAbout(resp);
-	} else { 
 		
- 		VK.Auth.login(pushAbout(function(response){
+	  } else { 
+		
+ 	    VK.Auth.login(pushAbout(function(response){
 			
-			pushAbout(response);
+		  pushAbout(response);
+		  
 		}),2);
 	} 
 });  
@@ -23,40 +25,39 @@ var pushAbout = function(response){
 	
 	if (response.session) {
 		      
-			  console.log(response);
-			  
-			  /*$(".you").text("Ваше имя :"+response.session.user.first_name+" "+
-			                              response.session.user.last_name );*/
+			 
 			  
 	     
 			  VK.Api.call('friends.get', {fields: ['uid', 'first_name', 'last_name','crop_photo'], order: 'name',v:"5.73"}, function(r){
 			
-				console.log(r);
+				
 			
-				if(r.response) {
-			      $(".yourFrends").text("ваши друзья :") ;
-				  r = r.response.items;
-			  
-				  for( var i = 0 ; i < Math.min(r.length,5) ; ++i ){
-			 
+				  if(r.response) {
+					  
+					  $(".yourFrends").text("ваши друзья :") ;
 					
-				
-                    if(r[i].crop_photo) 
-			        pushCard(r[i].crop_photo.photo.photo_807,r[i].first_name,r[i].last_name);
-					else
-					pushCard("https://vk.com/images/camera_100.png?ava=1",r[i].first_name,r[i].last_name);
-				
-				  }
-			  
-			   }else alert("Не удалось получить список ваших друзей(((");
+					  r = r.response.items;
+				  
+					  for( var i = 0 ; i < Math.min(r.length,5) ; ++i ){
+				 
+						
+					
+						if(r[i].crop_photo) 
+						pushCard(r[i].crop_photo.photo.photo_807,r[i].first_name,r[i].last_name);
+						else
+						pushCard("https://vk.com/images/camera_100.png?ava=1",r[i].first_name,r[i].last_name);
+					
+					  }
+				  
+				  }else alert("Не удалось получить список ваших друзей(((");
 			 
 			  })
 		       
 			  VK.Api.call('users.get',{fields: ['first_name', 'last_name'], order: 'name',v:"5.73"},function(r){
 				  
-				console.log(r);
-				$(".you").text("Ваше имя :"+r.first_name+" "+
-			                                r.last_name );
+				
+				 $(".you").text("Ваше имя :"+r[0].first_name+" "+
+			                                 r[0].last_name );
 				
 			  }) 
 			   
