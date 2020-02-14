1c1125360d682b9f7fd59a1e908a10549ebee226
Vlad <yavlads2014@yandex.ru>
2020-02-14 12:17:56 +0300

    рефакторинг

--- src/Controller/AddController.php
+++ src/Controller/AddController.php
@@ -1,49 +1,50 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
- 
+use Symfony\Component\Routing\Annotation\Route;
+
 class AddController extends AbstractController
 {
     /**
      * @Route("/add", name="add")
      */
-    public function index(Request $request  )
-    {   
+    public function index(Request $request)
+    {
         $em = $this->getDoctrine()->getManager();
-         
-		$book = new Book();
+
+        $book = new Book();
         $book->setName('book name');
         $book->setYear('2020');
         $book->setAuthor('Author');
-		$form = $this->createFormBuilder($book)
-			->add('name', TextType::class)
+        $form = $this->createFormBuilder($book)
+            ->add('name', TextType::class)
             ->add('author', TextType::class)
-			->add('year',  NumberType::class, [
+            ->add('year', NumberType::class, [
                 'required' => true,
-                'attr'     => array(
-                    'min'  => 0,
-                    'max'  => 3000,
-                ) 
+                'attr' => array(
+                    'min' => 0,
+                    'max' => 3000,
+                ),
             ])
-			->getForm();
-		$form->handleRequest($request);
+            ->getForm();
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid()) {
-             
-			$data = $form->getData();
-			$em->persist($book);
+        if ($form->isSubmitted() && $form->isValid()) {
 
-			$em->flush();
+            $data = $form->getData();
+            $em->persist($book);
+
+            $em->flush();
 
             return $this->redirectToRoute('list');
-        
-		}
+
+        }
         return $this->render('add/index.html.twig', [
             'form' => $form->createView(),
         ]);
--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,11 @@ class ListController extends AbstractController
      * @Route("/list", name="list")
      */
     public function index()
-    {   
+    {
         $repository = $this->getDoctrine()->getRepository(Book::class);
         $books = $repository->findAll();
         return $this->render('list/index.html.twig', [
-            'books' => $books
+            'books' => $books,
         ]);
     }
 }
