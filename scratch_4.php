8773348295a86574c725664866277f6dc908aa70
Vlad <yavlads2014@yandex.ru>
2020-03-15 11:47:38 +0300

    first commit

---
+++ scratch_4.php
@@ -0,0 +1,27 @@
+<?php
+
+if (isset($_POST['order_id'])) {
+    $orderId = $_POST['order_id'];
+    if ($order = DB::query('SELECT o1.`order_id`, o2.`order_status`
+        FROM `orders` o1
+                 INNER JOIN `orders` o2 ON o2.`order_id` = o1.`order_id`
+        WHERE o1.`order_id` = {$orderId}' ) {
+        sendJson([
+            'status'       => 'success';
+            'order_id'     => $order['order_id'],
+            'order_status' => $order['status'],
+        ]);
+    }
+
+    sendJson(
+        [
+            'status'  => 'fail',
+            'message' => sprintf('Заказ с ID `%x` не найден', $orderId);
+        ]
+    );
+}
+
+function sendJson($array)
+{
+    print(json_decode($array));
+}
