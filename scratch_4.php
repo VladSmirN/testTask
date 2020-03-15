6e6108079350a5ae53d24d913dffba37d25b333a
Vlad <yavlads2014@yandex.ru>
2020-03-15 12:12:33 +0300

    Поменял кавычки для вставки orderId

--- scratch_4.php
+++ scratch_4.php
@@ -2,10 +2,10 @@
 
 if (isset($_POST['order_id'])) {
     $orderId = $_POST['order_id'];
-    if ($order = DB::query('SELECT o1.`order_id`, o2.`order_status`
+    if ($order = DB::query("SELECT o1.`order_id`, o2.`order_status`
         FROM `orders` o1
                  INNER JOIN `orders` o2 ON o2.`order_id` = o1.`order_id`
-        WHERE o1.`order_id` = {$orderId}' ) {
+        WHERE o1.`order_id` = {$orderId}") {
         sendJson([
             'status'       => 'success';
             'order_id'     => $order['order_id'],
