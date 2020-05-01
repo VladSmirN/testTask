e7d4d00879f444bddc0cf52e3f689fdef1d34e0c
Vlad <yavlads2014@yandex.ru>
2020-05-01 22:06:59 +0300

    Отображение отзывов

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,10 @@
-require('./bootstrap');
+//require('./bootstrap');
 window.Vue = require('vue');
-
-
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify);
 Vue.component('show-component', require('./components/ShowComponent.vue').default);
-
+ 
 const app = new Vue({
     el: '#app',
 });
