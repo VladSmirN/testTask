86895bde389f69486c2d099721c84b33bfdd5e77
Vlad <yavlads2014@yandex.ru>
2020-05-01 22:06:38 +0300

    Отображения отзывов

--- resources/js/components/ShowComponent.vue
+++ resources/js/components/ShowComponent.vue
@@ -1,23 +1,55 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+  <v-container>
+    <v-card v-for="(review,index) in reviews" v-bind:key="review.COMMENT" style="margin:10px" max-width="600">
+      <v-card-title>
+        {{review.NAME}}
+      </v-card-title>
+      <v-card-subtitle>
+        {{review.EMAIL}}
+      </v-card-subtitle>
+      <v-card-text>
+        {{review.COMMENT}}
+      </v-card-text>
+    </v-card>
+    <v-card max-width="600" style="margin:10px">
+      <v-card-text>
+        <v-text-field v-model="email" label="Имя" required></v-text-field>
+        <v-text-field v-model="name" label="E-mail" required></v-text-field>
+        <v-textarea
+        solo
+        v-model="comment"
+        name="input-7-4"
+        label="Отзыв"
+      ></v-textarea>
+        <v-btn block color="#2196F3" dark @click=send>
+          Добавить
+        </v-btn>
+      </v-card-text>
+    </v-card>
+  </v-container>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </template>
-
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+  import axios from 'axios'
+  export default {
+    mounted() {
+      console.log('Component mounted.')
+    },
+    data: () => ({
+      email: "",
+      name: "",
+      comment: "",
+      reviews: [{ NAME: "Vlad Vlad", EMAIL: "email@email.ru", COMMENT: "all ok" }]
+    }),
+    methods: {
+      async send(){
+        let  ans  = await axios.post('http://127.0.0.1:8000/reviews/store',{
+          email:"1",
+          name:"sasafsfa1",
+          comment:"1"
+        })
+        console.log(ans);
+      }
     }
+  }
 </script>
