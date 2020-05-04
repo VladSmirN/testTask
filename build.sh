890b8b3922d2acf888d66e149aa6014be96e5905
Vlad <yavlads2014@yandex.ru>
2020-05-05 00:13:41 +0300

    устонвка приложения

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 sudo chmod -R 777 ./storage
 sudo chmod -R 755 ./public
-
-sudo docker-compose up  --build
-
+composer install 
+sudo docker-compose up -d --build mysql
+php artisan migrate:refresh
+php artisan migrate:refresh
+php artisan serve
