9172e18c006f22af5a6b415c5219901b4d23cd04
Vlad <yavlads2014@yandex.ru>
2020-05-04 18:27:30 +0300

    add exampl env

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 sudo chmod -R 777 ./storage
 sudo chmod -R 755 ./public
 
-echo "start building"
+sudo docker-compose up  --build
 
-sudo docker-compose up -d --build
-
-sudo docker-compose exec fpm cp .env.example .env && php artisan key:generate
-
-echo "end building"
